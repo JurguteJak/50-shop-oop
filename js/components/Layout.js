ff480aa3a526b9571994a4006035e603b43d56a6
JurguteJak <jurgutejak@gmail.com>
2024-07-28 19:40:50 +0300

    responsive layout system

--- js/components/Layout.js
+++ js/components/Layout.js
@@ -26,33 +26,37 @@ export class Layout {
                 <h1 class="col-12 main-title">Pirmas reikalas</h1>
             </section>
             <section class="row">
-                <div class="col-12">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugiat perferendis similique voluptatibus sequi, deleniti, iusto  </div>
+                <div class="col-10 col-sm-8 col-md-6 col-lg-4 col-xl-2 col-xxl-1">P</div>
+                <div class="col-1 m-1 m-sm-3 col-md-2 m-md-4 col-lg-4 m-xl-6 col-xxl-1 m-xxl-10">P</div>
             </section>
             <section class="row">
-                <div class="col-6">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugiat perferendis similique voluptatibus sequi, deleniti, iusto   </div>
-                <div class="col-6">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugiat perferendis similique voluptatibus sequi, deleniti, iusto   </div>
+                <div class="col-9">Pirmas reikalas</div>
+                <div class="col-2 m-1">Pirmas reikalas</div>
             </section>
             <section class="row">
-                <div class="col-3">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugiat perferendis similique voluptatibus sequi, deleniti, iusto   </div>
-                <div class="col-3">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugiat perferendis similique voluptatibus sequi, deleniti, iusto   </div>
-                <div class="col-3">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugiat perferendis similique voluptatibus sequi, deleniti, iusto   </div>
-                <div class="col-3">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugiat perferendis similique voluptatibus sequi, deleniti, iusto   </div>
+                <div class="col-8">Pirmas reikalas</div>
+                <div class="col-3 m-1">Pirmas reikalas</div>
             </section>
             <section class="row">
-                <div class="col-6">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugiat perferendis similique voluptatibus sequi, deleniti, iusto   </div>
-                <div class="col-3">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugiat perferendis similique voluptatibus sequi, deleniti, iusto   </div>
-                <div class="col-3">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugiat perferendis similique voluptatibus sequi, deleniti, iusto   </div>
+                <div class="col-7">Pirmas reikalas</div>
+                <div class="col-4 m-1">Pirmas reikalas</div>
             </section>
             <section class="row">
-                <div class="col-3">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugiat perferendis similique voluptatibus sequi, deleniti, iusto   </div>
-                <div class="col-6">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugiat perferendis similique voluptatibus sequi, deleniti, iusto   </div>
-                <div class="col-3">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugiat perferendis similique voluptatibus sequi, deleniti, iusto   </div>
+                <div class="col-6">Pirmas reikalas</div>
+                <div class="col-4 m-2">Pirmas reikalas</div>
             </section>
             <section class="row">
-                <div class="col-3">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugiat perferendis similique voluptatibus sequi, deleniti, iusto   </div>
-                <div class="col-3">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugiat perferendis similique voluptatibus sequi, deleniti, iusto   </div>
-                <div class="col-6">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugiat perferendis similique voluptatibus sequi, deleniti, iusto   </div>
+                <div class="col-11">Pirmas reikalas</div>
             </section>
+            <section class="row">
+                <div class="col-12 col-sm-6 col-lg-4 col-xl-3 col-xxl-12">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugiat perferendis similique voluptatibus sequi, deleniti, iusto  </div>
+                <div class="col-12 col-sm-6 col-lg-4 col-xl-3">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugiat perferendis similique voluptatibus sequi, deleniti, iusto  </div>
+                <div class="col-6 col-lg-4 col-xl-3">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugiat perferendis similique voluptatibus sequi, deleniti, iusto  </div>
+                <div class="col-6 col-lg-4 col-xl-3">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugiat perferendis similique voluptatibus sequi, deleniti, iusto  </div>
+                <div class="col-12 col-md-6 col-lg-4 col-xl-3 col-xxl-1">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugiat perferendis similique voluptatibus sequi, deleniti, iusto  </div>
+                <div class="col-12 col-md-6 col-lg-4 col-xl-3 col-xxl-">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugiat perferendis similique voluptatibus sequi, deleniti, iusto  </div>
+            </section>
+            
         </main>`;
         return HTML;
     }
